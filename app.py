c1708f55a1240c5664738237916674364148bb15
bankadisorn77 <121882001+bankadisorn77@users.noreply.github.com>
2025-04-20 16:00:42 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pickle
+import matplotlib.pyplot as plt
+
+#load model
+with open('kmeans_model.pkl','rb') as f:
+    model = pickle.load(f)
+    
+#set pafe cofig
+st.set_page_config(page_title = 'k-Means Clustering App', layout = 'centered')
+#set title
+st.title("k-Means Clustering Visualizer")
+#Diasplay
+st.subheader('Example Data for Visualization')
+st.markdown('This demo user example data (2D) to illustrate clustering results.')
+
+#load form a saved dataset
+
+from sklearn.datasets import make_blobs
+X, y = make_blobs(n_samples=300, centers=model.n_clusters, cluster_std=0.60, random_state=0)
+y_kmeans = model.predict(X)
