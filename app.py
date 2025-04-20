21cf28d517f427ab4b0fe70d0410e2b3d1911dff
bankadisorn77 <121882001+bankadisorn77@users.noreply.github.com>
2025-04-20 16:32:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
 import streamlit as st
 import pickle
 import matplotlib.pyplot as plt
-
+from sklearn.datasets import make_blobs
 #load model
 with open('kmeans_model.pkl','rb') as f:
     model = pickle.load(f)
     
 #set pafe cofig
-st.set_page_config(page_title = 'k-Means Clustering App', layout = 'centered')
+st.set_page_config(page_title = 'K-Means Clustering App', layout = 'centered')
 #set title
-st.title("k-Means Clustering Visualizer")
+st.title("k-Means Clustering Visualizer by Adisorn Saard")
 #Diasplay
 st.subheader('Example Data for Visualization')
 st.markdown('This demo user example data (2D) to illustrate clustering results.')
+#cluster = st.slider('number of cluster',2,10)
 
 #load form a saved dataset
+X, _ = make_blobs(n_samples=300, centers=model.n_clusters, cluster_std=0.60, random_state=0)
 
-from sklearn.datasets import make_blobs
-X, y = make_blobs(n_samples=300, centers=model.n_clusters, cluster_std=0.60, random_state=0)
+#Prideict
 y_kmeans = model.predict(X)
 
-# Plotting
-plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, cmap='viridis')
-plt.scatter(loaded_model.cluster_centers_[:, 0], model.cluster_centers_[:, 1], s=300, c='red')
-plt.title('k-Means Clustering')
-plt.show()
+fig, ax = plt.subplots()
+scatter = ax.scatter(X[:, 0], X[:, 1], c=y_kmeans, cmap='viridis')
+ax.scatter(model.cluster_centers_[:, 0], model.cluster_centers_[:, 1], s=300, c='red')
+ax.set_title('k-Means Clustering')
+ax.legend()
+st.pyplot(fig)
